d14449df7b5500c8ea1c8ea31f5d92cec42e73c5
Nereid3611 <71778576+Nereid3611@users.noreply.github.com>
2020-10-08 18:15:04 +0900

    Initial commit

---
+++ Assets/sstarC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sstarC : MonoBehaviour
+{
+    private float rotspeed = 3.0f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, this.rotspeed, 0);
+    }
+}
